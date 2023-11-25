b1a6d381ecdfdd7ab78e605a9cba45cb8a79a852
Daniel Rodriguez <127802925+danielrodc@users.noreply.github.com>
2023-11-25 01:17:16 +0000

    button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,6 +4,21 @@ import Light from "./light.jsx";
 //create your first component
 const Home = () => {
   const [active, setActive] = useState(null);
+
+  const handleButtonClick = () => {
+    const lightSequence = ['red', 'yellow', 'green'];
+    let activeIndex = 0;
+
+    const intervalId = setInterval(() => {
+      setActive(lightSequence[activeIndex]);
+      activeIndex++;
+
+      if (activeIndex === lightSequence.length) {
+        clearInterval(intervalId);
+        setActive(null);
+      }
+    }, 1000);
+  }
   return (
     <>
 	<body>
@@ -11,6 +26,7 @@ const Home = () => {
         className="container m-auto mt-5 pt-5 pb-5 bg-dark col-2 rounded d-flex flex-column align-items-center"
         onClick={() => setActive(event.target.style.backgroundColor)}
       >
+        <button className="bg-dark" onClick={handleButtonClick}>Click!</button>
         <Light color="red" active={active} />
         <Light color="yellow" active={active} />
         <Light color="green" active={active} />
